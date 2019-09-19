8068bfe0103694e2d1dcbddc5ed594dd847ab2e9
GabrielBrunatti <55466917+GabrielBrunatti@users.noreply.github.com>
2019-09-18 21:27:08 -0300

    ProvaMartinnn

---
+++ Projeto/Dados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    class Dados
+    {
+        public string Id { get; set; }
+        public string Genero { get; set; }
+        public string Nome { get; set; }
+        public string Data { get; set; }
+        public float Segurança { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Id:" + Id + " Genero:" + Genero + " Nome:" + Nome + " Data:" + Data + " Numero de segurança:" + Segurança;
+        }
+
+
+
+
+
+    }
+}
